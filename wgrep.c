a24c280e6e5594f3ae082d491c4a207da7575489
macnoblebk <macnoblebk@gmail.com>
2023-07-02 12:14:16 -0700

    Implements additional error checking logic and loops through all files if multiple files are passed.

--- wgrep.c
+++ wgrep.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-   if (argc == 1){
-      printf("wgrep:searchterm [file...] ");
+   if (argc == 1) {
+      printf("wgrep:searchterm [file...]\n");
       return 1;
 
+   }
+   
+   if (argc == 2) {
+   // use fgets to read, input from stdin and match search term
+      return 0;
+   }
+
+   char* filename;
+   File *fp = NULL;
+
+   for (int i = 2; i < argc; i++) {
+
+      filename = argv[i];
+
+      fp = fopen(filename, 'r');
+
+      if (fp == NULL) {
+         printf("wgrep:cannot open file.\n");
+      } 
+
+        
    }
    
 
