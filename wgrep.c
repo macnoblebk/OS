d51d716b29c78ac8ab929ca39d36421d4237c484
macnoblebk <macnoblebk@gmail.com>
2023-07-02 13:52:03 -0700

    Handles logic if textfile is absent

--- wgrep.c
+++ wgrep.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
    if (argc == 1) {
@@ -8,31 +9,40 @@ int main(int argc, char* argv[]) {
 
    }
    
+   char* search_term = argv[1];
+   char buffer[256];
+   char* line = NULL;
+   size_t linecap = 0;
+   
    if (argc == 2) {
-   // use fgets to read, input from stdin and match search term
+      fgets(buffer, sizeof(buffer), stdin);
+
+      if (strstr(buffer, search_term) != NULL)
+         printf("%s", buffer);
+   
       return 0;
    }
 
-   char* line = NULL;
-   size_t linecap = 0;
+   else {
+         for (int i = 2; i < argc; i++) {
 
-   char* search_term = argv[1];
+         char *filename = argv[i];
 
-   for (int i = 2; i < argc; i++) {
+         FILE *fp = fopen(filename, "r");
 
-      char *filename = argv[i];
-      FILE *fp = fopen(filename, "r");
-
-      if (fp == NULL) {
-         printf("wgrep:cannot open file.\n");
-      } 
+         if (fp == NULL) 
+            printf("wgrep:cannot open file.\n");
+          
       
-      while (getline(&line, &linecap, fp)) {
-         if (strstr(line, search_term) != NULL)
-           printf("%s", line);
+         while (getline(&line, &linecap, fp)) {
+            if (strstr(line, search_term) != NULL)
+              printf("%s", line);
+         }
+
+         fclose(fp);
       }
 
-      fclose(fp);
+      free(line);
    }
    
    return 0;
