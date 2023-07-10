7bd1fdbb00748158fd0ecec686cb219dc0a48423
macnoblebk <macnoblebk@gmail.com>
2023-07-10 12:54:25 -0700

    Refactors wgrep program

--- wgrep.c
+++ wgrep.c
@@ -2,48 +2,45 @@
 #include <string.h>
 #include <stdlib.h>
 
+void wgrep(FILE *fp, const char *searchTerm) {
+   char *line = NULL;
+   size_t len = 0;
+ 
+   while (getline(&line, &len, fp) != -1){
+      if (strstr(line, searchTerm) != NULL)
+         printf("%s", line);
+   }
+
+   free(line);
+
+}
+
 int main(int argc, char* argv[]) {
-   if (argc == 1) {
-      printf("wgrep:searchterm [file...]\n");
+  
+   if (argc < 2) {
+      printf("wgrep: searchterm [file...]\n");
       return 1;
-
    }
    
-   char* search_term = argv[1];
-   char buffer[256];
-   char* line = NULL;
-   size_t linecap = 0;
+   const char* searchTerm = argv[1]; 
    
-   if (argc == 2) {
-      fgets(buffer, sizeof(buffer), stdin);
-
-      if (strstr(buffer, search_term) != NULL)
-         printf("%s", buffer);
+   if (argc == 2)
+      wgrep(stdin, searchTerm); 
    
-      return 0;
-   }
-
    else {
-         for (int i = 2; i < argc; i++) {
-
-         char *filename = argv[i];
-
-         FILE *fp = fopen(filename, "r");
-
-         if (fp == NULL) 
-            printf("wgrep:cannot open file.\n");
-          
-      
-         while (getline(&line, &linecap, fp)) {
-            if (strstr(line, search_term) != NULL)
-              printf("%s", line);
-         }
-
-         fclose(fp);
-      }
-
-      free(line);
-   }
+      for (int i = 2; i < argc; i++) {
    
+        FILE *fp = fopen(argv[i], "r");
+
+        if (fp == NULL) { 
+           printf("wgrep: me cannot open file\n");
+           return 1;
+        }
+
+        wgrep(fp, searchTerm);
+        fclose(fp);
+      }
+   }
+
    return 0;
 }
