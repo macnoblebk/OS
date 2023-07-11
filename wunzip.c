6d30bf000ba23ca7ab6665768a2c7b3bbbabeeae
macnoblebk <macnoblebk@gmail.com>
2023-07-11 13:37:34 -0700

    Refactors wzip and wunzip program

--- wunzip.c
+++ wunzip.c
@@ -4,11 +4,16 @@
       char character;
       int count;
       
-      if (argc  < 2)
+      if (argc  < 2) {
          printf("wunzip: file [file2 ...]\n");
+         return 1;
+      }
 
       for(int i = 1; i < argc; i++) {
          FILE *fp = fopen(argv[i], "rb");
+
+         if (fp == NULL)
+            printf("wunzip: cannot open file");
 
          while (fread(&count, sizeof(int), 1, fp) > 0) {
             fread(&character, sizeof(char), 1, fp);
@@ -18,6 +23,9 @@
             }
 
          }
+
+         fclose(argv[i]);
+      }
 
       return 0;
    }
--- wzip.c
+++ wzip.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 
-   int main(int argc, char* argv[]) {
-      if (argc < 2)
-         printf("wzip: file [file2 ...]\n");       
+   int main (int argc, char* argv[]) {
+      if (argc < 2) {
+         printf("wzip: file [file2 ...]\n");
+         return 1;
+      }       
       
-      else {
-         for (int i = 1; i < argc; i++) {
-            FILE *fp = fopen(argv[i],"rb");
-            char prevChar, currChar;
-            int count = 1;
-   
-            fread(&prevChar, sizeof(char), 1, fp);
-            while(fread(&currChar, sizeof(char), 1, fp) > 0) {
-               if (currChar == prevChar)
-                   count++;
-               
-               else {
-                  fwrite(&count, sizeof(int), 1, stdout);
-                  fwrite(&prevChar, size(char), 1, stdout);
-                  prevChar = currChar;
-                  count = 1;
-               }
-            }
+      for (int i = 1; i < argc; i++) {
+         FILE *fp = fopen(argv[i],"rb");
 
-            fwrite(&count, sizeof(int), 1, stdout);
-            fwrite(&prevChar, sizeof(char), 1, stdout);
+         if (fp == NULL)
+             printf("wzip: cannot open file");
+         char prevChar, currChar;
+         int count = 1;
+   
+         fread(&prevChar, sizeof(char), 1, fp);
+         while(fread(&currChar, sizeof(char), 1, fp) > 0) {
+            if (currChar == prevChar)
+                count++;
+               
+            else {
+               fwrite(&count, sizeof(int), 1, stdout);
+               fwrite(&prevChar, size(char), 1, stdout);
+               prevChar = currChar;
+               count = 1;
+            }
+         }
+
+         fwrite(&count, sizeof(int), 1, stdout);
+         fwrite(&prevChar, sizeof(char), 1, stdout);
 	    
-            fclose(fp);
-            fclose(fpout);
-        }
+         fclose(fp);
+      }
 
      }
 
